0354244b492e9ace525b4d3743a90ee836aee5b5
Matteo Caprari <matteo.caprari@gmail.com>
2013-09-26 07:43:08 +0100

    Fixed resources url in giddone.js

--- giddone-web-ui/giddone.js
+++ giddone-web-ui/giddone.js
@@ -1,9 +1,9 @@
 function MainCtrl($scope) {
-    $scope.mainUrl = 'main.html';
+    $scope.mainUrl = '/giddone/main.html';
 }
 function TodoCtrl($scope) {
     $scope.snapshot = snapshot();
     $scope.todos = _.groupBy($scope.snapshot.todo, function(todo) { return todo.file; });
 }
 // TODO: underscore is only used for the group_by and should be removed
-document.write('<link href="giddone.css" media="all" rel="stylesheet" type="text/css"><link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"><\/script><script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"><\/script>');
+document.write('<link href="/giddone/giddone.css" media="all" rel="stylesheet" type="text/css"><link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"><\/script><script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"><\/script>');
